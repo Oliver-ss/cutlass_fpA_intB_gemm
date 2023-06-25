2af2a4029b078884f37be1190e624e36ef818666
oliveryuan <oliveryuan@basemind.com>
2023-06-25 16:42:39 +0800

    add torch op

---
+++ cutlass_kernels/fused_gemm_dq.cc
@@ -0,0 +1,99 @@
+#include <cublas_v2.h>
+#include <iostream>
+#include <vector>
+
+#include "torch/csrc/cuda/Stream.h"
+#include <torch/custom_class.h>
+#include <torch/script.h>
+
+#include "cutlass_kernels/fpA_intB_gemm/fpA_intB_gemm.h"
+#include "cutlass_kernels/th_utils.h"
+
+#include "cutlass/numeric_types.h"
+
+using torch::Tensor;
+
+namespace torch_ext {
+
+
+template<typename T, typename WeightType>
+Tensor fused_gemm_dq_helper(
+    Tensor input_activations, Tensor weight, Tensor scales)
+{
+    const at::ScalarType _st    = input_activations.scalar_type();
+    const int            m      = input_activations.size(0);
+    const int            n      = scales.size(0);
+    const int            k      = input_activations.size(1);
+    auto                 stream = at::cuda::getCurrentCUDAStream().stream();
+
+    const T*          input_act_ptr = get_ptr<const T>(input_activations);
+    const WeightType* weight_ptr    = get_ptr<const WeightType>(weight);
+    const T*          scales_ptr    = get_ptr<const T>(scales);
+
+    fastertransformer::CutlassFpAIntBGemmRunner<T, WeightType> fused_gemm_dq_runner;
+    const int ws_bytes = fused_gemm_dq_runner.getWorkspaceSize(m, n, k);
+
+    auto output_tensor = torch::empty({m, n}, torch::dtype(_st).device(torch::kCUDA).requires_grad(false));
+    auto ws_tensor     = torch::empty({ws_bytes}, torch::dtype(torch::kInt8).device(torch::kCUDA).requires_grad(false));
+
+    T*   output_tensor_ptr = get_ptr<T>(output_tensor);
+    char* ws_ptr            = get_ptr<char>(ws_tensor);
+
+    fused_gemm_dq_runner.gemm(input_act_ptr, weight_ptr, scales_ptr, output_tensor_ptr, m, n, k, ws_ptr, ws_bytes, stream);
+    return output_tensor;
+}
+
+Tensor fused_gemm_dq(Tensor input_activations, Tensor weight, Tensor scales)
+{
+    const at::ScalarType _st = input_activations.scalar_type();
+    CHECK_INPUT(scales, _st);
+
+    TORCH_CHECK(input_activations.dim() == 2, "Invalid rank for activations");
+    TORCH_CHECK(weight.dim() == 2, "Invalid rank for weight");
+    TORCH_CHECK(scales.dim() == 1, "Invalid rank for scales");
+
+    const int m = input_activations.size(0);
+    const int n = scales.size(0);
+    const int k = input_activations.size(1);
+
+    TORCH_CHECK(input_activations.size(1) == weight.size(0), "dim 1 of act and dim 0 of weight must be equal");
+
+    // We signal int4 by having the last weight dim be half the size of the scales.
+    // This is because int4 elements are packed into a single byte.
+    torch::ScalarType quant_type = weight.scalar_type();
+    if (weight.size(-1) == scales.size(-1) / 2) {
+        quant_type = at::ScalarType::QUInt4x2;
+    }
+    else {
+        TORCH_CHECK(weight.size(-1) == scales.size(-1),
+                    "Last dim of weight and scales must be equal for int8 "
+                    "or last dim of scale must be 2x last dim of weight for int4.");
+    }
+
+    Tensor output_tensor;
+    switch (_st) {
+        case at::ScalarType::Half: {
+            if (quant_type == torch::kInt8) {
+                output_tensor =
+                    fused_gemm_dq_helper<half, uint8_t>(input_activations, weight, scales);
+            }
+            else if (quant_type == at::ScalarType::QUInt4x2) {
+                output_tensor = fused_gemm_dq_helper<half, cutlass::uint4b_t>(
+                    input_activations, weight, scales);
+            }
+            else {
+                std::string err_msg = "Unsupported weight type " + std::string(at::toString(quant_type));
+                throw std::runtime_error(err_msg);
+            }
+            break;
+        }
+        default:
+            throw std::runtime_error("Unsupported tensor type. Got " + std::string(at::toString(_st)));
+    }
+    return output_tensor;
+
+TORCH_LIBRARY(gemm_dq_unit_ops, m)
+{
+    m.def("fused_gemm_dq", fused_gemm_dq);
+}
+}
---
+++ cutlass_kernels/th_utils.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <torch/extension.h>
+#include "torch/csrc/cuda/Stream.h"
+#include <ATen/cuda/CUDAContext.h>
+#include <cstdio>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#include <iostream>
+#include <nvToolsExt.h>
+#include <vector>
+
+#define CHECK_TYPE(x, st) TORCH_CHECK(x.scalar_type() == st, "Inconsistency of Tensor type: " #x)
+#define CHECK_TH_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CPU(x) TORCH_CHECK(!x.is_cuda(), #x " must be a CPU tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x, st)                                                                                             \
+    CHECK_TH_CUDA(x);                                                                                                  \
+    CHECK_CONTIGUOUS(x);                                                                                               \
+    CHECK_TYPE(x, st)
+#define CHECK_CPU_INPUT(x, st)                                                                                         \
+    CHECK_CPU(x);                                                                                                      \
+    CHECK_CONTIGUOUS(x);                                                                                               \
+    CHECK_TYPE(x, st)
+#define CHECK_OPTIONAL_INPUT(x, st)                                                                                    \
+    if (x.has_value()) {                                                                                               \
+        CHECK_INPUT(x.value(), st);                                                                                    \
+    }
+#define CHECK_OPTIONAL_CPU_INPUT(x, st)                                                                                \
+    if (x.has_value()) {                                                                                               \
+        CHECK_CPU_INPUT(x.value(), st);                                                                                \
+    }
+#define PRINT_TENSOR(x) std::cout << #x << ":\n" << x << std::endl
+#define PRINT_TENSOR_SIZE(x) std::cout << "size of " << #x << ": " << x.sizes() << std::endl
+
+namespace torch_ext {
+
+template<typename T>
+inline T* get_ptr(torch::Tensor& t)
+{
+    return reinterpret_cast<T*>(t.data_ptr());
+}
+}  // namespace torch_ext
